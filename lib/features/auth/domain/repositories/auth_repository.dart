0cbf837ce464cf5cb10a1823d80312d4e6edc492
EgorkaKv <yehor.kvasenko@gmail.com>
2026-04-27 00:11:03 +0300

    feat: add plug for auth_repo and storage_service

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,7 @@
+abstract class AuthRepository {
+  Future<void> register({required String email, required String password});
+
+  Future<void> login({required String email, required String password});
+
+  Future<void> logout();
+}
